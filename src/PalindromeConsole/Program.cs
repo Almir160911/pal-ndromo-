49198cf125d29129310ebb1e255f4748395e028f
kostyden <denys.kostynchuk@pp.inet.fi>
2017-07-19 21:03:46 +0300

    Introduced constant for amount of required palindromes

--- src/PalindromeConsole/Program.cs
+++ src/PalindromeConsole/Program.cs
@@ -6,12 +6,16 @@
 
     public class Program
     {
+        private const int REQUIRED_AMOUNT_OF_PALINDROMES = 3;
+
         public static void Main(string[] args)
         {
             var validator = new Palindrome.Validator();
 
             var word = args.FirstOrDefault();
-            var formattedResults = validator.GetFrom(word).Take(3).Select(result => result.Format());
+            var formattedResults = validator.GetFrom(word)
+                                            .Take(REQUIRED_AMOUNT_OF_PALINDROMES)
+                                            .Select(result => result.Format());
 
             Print(formattedResults);
         }     
