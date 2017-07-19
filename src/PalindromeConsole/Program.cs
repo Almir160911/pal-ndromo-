9b39a332bcfa9c02a3685030ed0094e83c81edcd
Denys Kostynchuk <denys.kostynchuk@kaski.fi>
2017-07-19 15:11:19 +0300

    Initial commit with acceptence tests

---
+++ src/PalindromeConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PalindromeConsole
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Text: hijkllkjih, Index: 23, Length: 10");
+            Console.WriteLine("Text: defggfed, Index: 13, Length: 8");
+            Console.WriteLine("Text: abccba, Index: 5 Length: 6");
+        }
+    }
+}
---
+++ tests/Palindrome.Tests/AcceptenceTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Palindrome.Tests
+{
+    using FluentAssertions;
+    using NUnit.Framework;
+    using PalindromeConsole;
+    using System;
+    using System.IO;
+    using System.Text;
+
+    [TestFixture]
+    class AcceptenceTests
+    {
+        [Test]
+        public void MainShouldPrintExpectedOutputBasedOnProvidedArgument()
+        {
+            var argument = "sqrrqabccbatudefggfedvwhijkllkjihxymnnmzpop";
+            var expectedOutputBuilder = new StringBuilder();
+            expectedOutputBuilder.AppendLine("Text: hijkllkjih, Index: 23, Length: 10");
+            expectedOutputBuilder.AppendLine("Text: defggfed, Index: 13, Length: 8");
+            expectedOutputBuilder.AppendLine("Text: abccba, Index: 5 Length: 6");
+            var expectedOutput = expectedOutputBuilder.ToString();
+
+            var actualOutputBuilder = new StringBuilder();
+            using (var writer = new StringWriter(actualOutputBuilder))
+            {
+                Console.SetOut(writer);
+                Program.Main(new[] { argument });
+            }
+
+            var actualOutput = actualOutputBuilder.ToString();
+            actualOutput.Should().Be(expectedOutput);
+        }
+    }
+}
